25ea462980f89e1a73366c4da7502336b45e7f7b
Ayebk <Ayebk123@gmail.com>
2024-10-26 18:27:15 +0300

    correcting BASE_URL

--- client/src/requestMethods.js
+++ client/src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BASE_URL = "https://ecommerce-food-now-dce4d2ac51b1.herokuapp.com/api/"; 
-const BASE_URL = "http://localhost:3000/api/"; 
+const BASE_URL = "https://ecommerce-food-now-dce4d2ac51b1.herokuapp.com/api/"; 
+// const BASE_URL = "http://localhost:3000/api/"; 
 
 
 // const BASE_URL = process.env.NODE_ENV === "production"
